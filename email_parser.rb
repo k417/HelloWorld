02b631daac41a3caba3057ac1757d7e002ff4873
Young <kyoung@txt.textron.com>
2019-07-10 18:19:11 -0400

    regex to parse text for email address domains, store in hash with freq

---
+++ email_parser.rb
@@ -0,0 +1,15 @@
+#This is file will parse the random_text.txt for emails address domains and output them
+#The email address domains will be stored in a Hash to keep track of the frequency of each domain match
+
+email_pattern = /\w\@(\w*.\w*)/
+email_matches = File.read("random_text.txt").scan(email_pattern)
+
+puts email_matches
+
+word_frequency = Hash.new(0)
+
+email_matches.each do |email|
+  word_frequency[email] += 1
+end
+
+puts word_frequency
