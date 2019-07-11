26dcf7e635c076cc4bdca5d4a9ec75820440a1f3
Young <kyoung@txt.textron.com>
2019-07-11 14:15:12 -0400

    moved project into its own folder

---
+++ EmailParser/email_parser.rb
@@ -0,0 +1,15 @@
+#This is file will parse the random_text.txt for emails address domains and output them
+#The email address domains will be stored in a Hash to keep track of the frequency of each domain match
+#The domain name frequency hash will be sorted alphabetically then looped through to output the results
+
+domain_name_pattern = /\w\@(\w*.\w*)/
+domain_name_matches = File.read("random_text.txt").scan(domain_name_pattern)
+
+frequency_hash = Hash.new(0)
+
+domain_name_matches.each do |domain|
+  frequency_hash[domain] += 1
+end
+
+frequency_hash.sort
+frequency_hash.each { |domain, freq| puts "#{domain[0].rjust(13)}: #{freq} time(s)"}
